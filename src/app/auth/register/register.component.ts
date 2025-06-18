24b315ec08a4c6682a122e62259eeda56195f54e
Dario Barreto <dario.barreto@gmail.com>
2025-06-18 16:46:00 -0500

    Changes on routes and

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registroForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.registroForm = this.formBuilder.group(
+      {
+        nombre: ['', Validators.required],
+        correo: ['', Validators.required, Validators.email],
+        password: ['', Validators.required]
+      });
+  }
+
+  ngOnInit() {
+
+  }
+  crearUsuario(){
+    console.log(this.registroForm);
+    console.log(this.registroForm.valid);
+    console.log(this.registroForm.value);
+  }
 }
